cd45f98c16c7a508c72afdd9d54640b853ba872a
michaeljmcardoso <michaelmiranda38@yahoo.com.br>
2025-02-28 10:42:23 -0300

    qrCodeBudd

--- QRcode.py
+++ QRcode.py
@@ -1,7 +1,7 @@
 import qrcode
 
-img = qrcode.make('http://www.yahoo.com.br')
+img = qrcode.make('https://play.google.com/store/apps/details?id=com.zumbiverde.barbuddybrasil&hl=pt_BR')
 print(type(img))
 print(img.size)
 
-img.save('qrcode_yahoo.png')
+img.save('qrcode_budd_app.png')
